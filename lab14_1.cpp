6459306886446dace42a61825e8b474009e25d64
Holytis <holytis123@gmail.com>
2023-01-13 16:09:32 +0700

    Ed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,24 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[])
+{
+    double sum = 0 , sum2 = 0 , mul = 1.0 , hm = 0 , max = 0 , min = 999 ;
+    for(int i=0; i<N ;i++)
+    {
+        sum = sum + A[i] ;
+        sum2 = sum2 + pow(A[i],2) ;
+        mul = mul * A[i] ;
+        hm = hm + (1.0/A[i]) ;
+        if (max <= A[i]) max = A[i] ;
+        else if (min >= A[i]) min = A[i] ;
+    }
+    B[0] = sum / N ;
+    B[1] = sqrt(((sum2/N))-(pow(B[0],2))) ;
+    B[2] = pow(mul , 1.0/N) ;
+    B[3] = N / hm ;
+    B[4] = max ;
+    B[5] = min ;
+
+}
